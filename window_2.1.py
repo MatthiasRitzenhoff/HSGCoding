1f79c7e1947f36b67426006fb057e20a750e901b
Syntactic Sugar <vanessarosemarie.zeh@student.unisg.ch>
2022-05-15 15:25:16 +0200

    Added new code to window 2.1

--- window_2.1.py
+++ window_2.1.py
@@ -16,7 +16,10 @@ window.geometry("530x310")
 Label(window, text="Provide the names of two movies either sperated by '&' if you want to retrieve all the actors,\n'|' if you want the actors that have been a part of both movie crews,\nor '^' if you all actors that have been only part of one of the movies.", bg="white", fg="black", font="non 10").grid(row=1, column=0)
 
 #Create label for movie #1
-Label(window, text="\nEnter the name of the first movie: ", bg="white", fg="black", font="non 10").grid(row=4, column=0)
+Label(window, text="Enter the name of the first movie: ", bg="white", fg="black", font="non 10").grid(row=4, column=0)
+
+def Value():
+    textentry=nameOfFstMovie.get()
 
 #Create entry label for movie #1
 textentry=Entry(window, width=50, bg="White")
@@ -25,6 +28,9 @@ textentry.grid(row=5, column=0)
 #Create label for operator
 Label(window, text="\nPick the operator you would like to apply: ", bg="white", fg="black", font="non 10").grid(row=7, column=0)
 
+def Value():
+    dd=operator.get()
+
 #Creating dropdown menu
 operator=["&", "|", ","]
 dd=ttk.Combobox(window,values=operator,width=10)
@@ -32,6 +38,9 @@ dd.grid(row=9,column=0,padx=10,pady=10)
 
 #Create label for movie #2
 Label(window, text="\nEnter the name of the second movie: ", bg="white", fg="black", font="non 10").grid(row=10, column=0)
+
+def Value():
+    textentry=nameOfSndMovie.get()
 
 #Create entry label for movie #2
 textentry=Entry(window, width=50, bg="White")
